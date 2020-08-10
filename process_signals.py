91ff797f4b615652bc8a32dd7c9e08e1720751e4
DiegoBulhoes <diegobulhoesm@gmail.com>
2020-08-10 19:56:34 -0300

    Refactory trading digital and binary

--- process_signals.py
+++ process_signals.py
@@ -28,18 +28,20 @@ class Metrics:
 
 
 class Bot(Metrics):
-    def __init__(self, path_config='config.json', path_signals="signals.csv"):
+    def __init__(self, path_config='config.json', path_signals="signals.csv", path_report="report.csv"):
         super().__init__()
         self.logger = self.log()
         self.config = self.read_the_config_file(path_config)
         self.API = IQ_Option(self.config['email'], self.config['password'])
         self.path_signals = path_signals
+        self.path_report = path_report
         self.profit = 0
         self.signals = None
         self.balance = None
         self.stop_gain = None
         self.stop_loss = None
         self.max_martingale = None
+        self.martingale = None
         self.wallet = None
         self.signals = None
         self.report = None
@@ -79,15 +81,16 @@ class Bot(Metrics):
 
     def set_config_porcess(self):
         self.balance = self.API.get_profile_ansyc()['balance']
-        self.wallet = self.balance * self.config['wallet']
+        self.wallet = self.balance
         self.stop_loss = self.wallet * self.config['pct_stop_loss']
         self.stop_gain = self.wallet * self.config['pct_stop_gain']
         self.max_martingale = self.config['max_martingale']
+        self.martingale = self.config['martingale']
         self.API.change_balance(self.config['balance_mode'])
         self.report = pd.DataFrame(
-            columns=['name_active', 'order_value', 'call_or_put', 'profit', 'time_order', 'time_exp',
-                     'stop_gain',
-                     'stop_loss'])
+            columns=['trading_instrument', 'name_active', 'order_value', 'call_or_put', 'profit', 'martingale',
+                     'count_martingale',
+                     'time_order', 'time_exp'])
 
     def load_signals(self):
         self.logger.info('Load signals')
@@ -121,81 +124,85 @@ class Bot(Metrics):
         name_active = str(signal_data[1])
         expiration_time = int(signal_data[2])
         call_or_put = signal_data[3].lower()
-        signal_value = round(self.config['wallet'] * self.config['pct_wallet'], 2)
+        signal_value = round(self.wallet * self.config['pct_wallet'], 2)
         operation_type = signal_data[4].lower()
         self.run_trader(name_active, signal_value, call_or_put, expiration_time, operation_type, time_now)
 
     def run_trader(self, name_active, order_value, call_or_put, exp_timer, operation_type, time_now):
         if operation_type == "b":
-            self.trading_binary(call_or_put, name_active, exp_timer, order_value, time_now)
+            self.trading_binary(call_or_put, name_active, exp_timer, order_value, time_now, 0)
         elif operation_type == "d":
             self.trading_digitals(call_or_put, name_active, exp_timer, order_value, time_now)
 
-    def trading_digitals(self, call_or_put, name_active, exp_timer, order_value, time_now):
-        for i in range(self.max_martingale + 1):
-            status, order_id = self.API.buy(order_value, name_active, call_or_put, exp_timer)
-            self.logger.info("Buy order id: {} status: {}".format(order_id, status))
-
-            if status:
-                order_profit = self.API.check_win_digital_v2(order_id)
-                self.profit += round(order_profit, 2)
-                order_value = round(order_value * 2, 2)
-                self.add_order_to_the_report(call_or_put, exp_timer, name_active, order_value, time_now)
-                if order_profit > 0:
-                    self.logger.info(
-                        "Profit order id: {}  order_profit: {} current profit: {} - WIN".format(order_id, order_profit,
-                                                                                                round(self.profit, 2)))
-                    break
-                self.stop()
-                self.logger.info("Order id:{} Loss: {}".format(order_id, order_profit))
-                self.logger.info("Current profit: {}".format(self.profit))
-                self.logger.info('Executing martingale')
-            else:
-                self.logger.error('Error placing order')
+    def trading_digitals(self, call_or_put, name_active, exp_timer, order_value, time_now, count_martingale,
+                         martingale=False):
+        status, order_id = self.API.buy(order_value, name_active, call_or_put, exp_timer)
+        self.logger.info("Buy order id: {} status: {}".format(order_id, status))
+        order_profit = round(self.API.check_win_digital_v2(order_id), 2)
+        self.profit += order_profit
+        self.add_order_to_the_report('digitals', call_or_put, exp_timer, name_active, order_value, time_now, martingale,
+                                     count_martingale)
+        if order_profit < 0 and self.martingale and self.max_martingale > count_martingale:
+            self.stop()
+            self.logger.info("Order id:{} Loss: {}".format(order_id, order_profit))
+            self.logger.info("Current profit: {}".format(self.profit))
+            self.logger.info('Executing martingale')
+            order_value = round(order_value * 2, 2)
+            count_martingale += 1
+            self.trading_binary(call_or_put, name_active, exp_timer, order_value, time_now, count_martingale,
+                                True)
+        else:
+            self.logger.info(
+                "Profit order id: {}  order_profit: {} current profit: {}".format(order_id, order_profit,
+                                                                                  round(self.profit, 2)))
         return call_or_put, name_active, exp_timer, order_value
 
-    def trading_binary(self, call_or_put, name_active, exp_timer, order_value, time_now):
-        for i in range(self.max_martingale + 1):
-            status, order_id = self.API.buy(order_value, name_active, call_or_put, exp_timer)
-            self.logger.info("Buy order id: {} status: {}".format(order_id, status))
-
-            if status:
-                order_profit = self.API.check_win_v3(order_id)
-                self.profit += round(order_profit, 2)
-                order_value = round(order_value * 2, 2)
-                self.add_order_to_the_report(call_or_put, exp_timer, name_active, order_value, time_now)
-                if order_profit > 0:
-                    self.logger.info(
-                        "Profit order id: {}  order_profit: {} current profit: {} - WIN".format(order_id, order_profit,
-                                                                                                round(self.profit, 2)))
-                    break
-                self.stop()
-                self.logger.info("Order id:{} Loss: {}".format(order_id, order_profit))
-                self.logger.info("Current profit: {}".format(self.profit))
-                self.logger.info('Executing martingale')
-            else:
-                self.logger.error('Error placing order')
+    def trading_binary(self, call_or_put, name_active, exp_timer, order_value, time_now, count_martingale,
+                       martingale=False):
+        status, order_id = self.API.buy(order_value, name_active, call_or_put, exp_timer)
+        self.logger.info("Buy order id: {} status: {}".format(order_id, status))
+        order_profit = round(self.API.check_win_v3(order_id), 2)
+        self.profit += order_profit
+        self.add_order_to_the_report('binary', call_or_put, exp_timer, name_active, order_value, time_now, martingale,
+                                     count_martingale)
+        if order_profit < 0 and self.martingale and self.max_martingale > count_martingale:
+            self.stop()
+            self.logger.info("Order id:{} Loss: {}".format(order_id, order_profit))
+            self.logger.info("Current profit: {}".format(self.profit))
+            self.logger.info('Executing martingale')
+            order_value = round(order_value * 2, 2)
+            count_martingale += 1
+            self.trading_binary(call_or_put, name_active, exp_timer, order_value, time_now, count_martingale,
+                                True)
+        else:
+            self.logger.info(
+                "Profit order id: {}  order_profit: {} current profit: {}".format(order_id, order_profit,
+                                                                                  round(self.profit, 2)))
         return call_or_put, name_active, exp_timer, order_value
 
-    def add_order_to_the_report(self, call_or_put, exp_timer, name_active, order_value, time_now):
+    def add_order_to_the_report(self, trading_instrument, call_or_put, exp_timer, name_active, order_value, time_now,
+                                martingale,
+                                count_martingale):
         time_order = time_now - timedelta(minutes=exp_timer)
-        self.report.loc[len(self.report) + 1] = [name_active, order_value, call_or_put, self.profit, time_order,
-                                                 time_now, self.stop_gain, self.stop_loss]
+        self.report.loc[len(self.report) + 1] = [trading_instrument, name_active, order_value, call_or_put,
+                                                 round(self.profit, 2), martingale, count_martingale, time_order,
+                                                 time_now]
 
     def process(self):
         FMT = '%H:%M:%S'
-        for signal_data in self.signals:
+        while len(self.signals) > 0:
             time_now = self.horary()
+            signal_data = self.signals[0]
             candle_size = int(signal_data[2]) * 60
             name_active = str(signal_data[1])
             signal_hour = signal_data[0]
-            self.logger.info('Analyzing signal: {}'.format(signal_data))
             if self.is_valid_order(FMT, time_now, signal_hour, candle_size, name_active):
                 wait_time = datetime.strptime(signal_hour, FMT) - datetime.strptime(time_now.strftime(FMT), FMT)
                 self.logger.info('Running signal: {}'.format(signal_data))
                 time.sleep(wait_time.seconds - 1)
+                self.signals.remove(signal_data)
                 self.execute_signal(signal_data, time_now)
-        self.report.to_csv('report.csv', index=False)
+        self.report.to_csv(self.path_report, index=False)
 
 
 if __name__ == '__main__':
